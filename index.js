39e15f870405e240a64dbc3974d775862e801f7f
Terrin Day <terrinday@gmail.com>
2018-09-11 21:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string)
-  {
+function sayHiToGrandma(string) 
     if (string.toLowerCase() === string) {
-    return "I can't hear you!";
-  } else if (string.toUpperCase() === string) {
+    return "I can't hear you!"}
+    else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
