aee9a3d5f248be60e806a69c8bf46bba398454f8
Terrin Day <terrinday@gmail.com>
2018-09-11 21:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function logShout(string)
 {
   console.log(string.toUpperCase())
 }
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+  return 
+  "I can't hear you!"
+  if 
+}
+}
